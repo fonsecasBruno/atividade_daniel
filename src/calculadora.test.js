cf754d2c65286a04d464205eae8faf4aabc8618e
Lucas de Lima Landim <127635545+LucasLandimLLL@users.noreply.github.com>
2024-09-02 21:47:45 -0300

    antes de terminar com erro

---
+++ src/calculadora.test.js
@@ -0,0 +1,31 @@
+const { default: expect } = require("expect");
+
+class Calculadora {
+    resultado;
+
+    constructor () {
+        this.resultado = 0;
+    }
+
+}
+
+describe("calculado", () => {
+
+    test("Criar nova calculadora", () => {
+        const calculadora = new Calculadora();
+    expect(calculadora).toBeDefined();
+    expect(calculadora.resultado).toBeDefined();
+    expect(typeof calculadora.resultado).toBe("number");
+    expect(calculadora.resultado).toBe(0);
+
+    });
+
+    it('Atribui um novo valor à calculadora', () => {
+        const calculadora = new Calculadora();
+        calculadora.resultado='Texto';
+        expect(() => {calculadora.resultado = 'Text'}).toThrow(TypeError);
+        expect(() => {calculadora.resultado = 'Texto'}).toThrow("O argumento deve ser um numero válido");
+
+    })
+
+});
