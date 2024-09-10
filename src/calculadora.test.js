d9044afd8e84fe36fbab382a198c1b0d78ce9377
Bruno Fonseca Souza <brunofsouza669@gmail.com>
2024-09-09 21:46:20 -0300

    atualização

--- src/calculadora.test.js
+++ src/calculadora.test.js
@@ -10,7 +10,6 @@ class Calculadora {
 }
 
 describe("calculado", () => {
-
     test("Criar nova calculadora", () => {
         const calculadora = new Calculadora();
     expect(calculadora).toBeDefined();
@@ -18,14 +17,21 @@ describe("calculado", () => {
     expect(typeof calculadora.resultado).toBe("number");
     expect(calculadora.resultado).toBe(0);
 
+
     });
 
     it('Atribui um novo valor à calculadora', () => {
         const calculadora = new Calculadora();
         calculadora.resultado='Texto';
         expect(() => {calculadora.resultado = 'Text'}).toThrow(TypeError);
-        expect(() => {calculadora.resultado = 'Texto'}).toThrow("O argumento deve ser um numero válido");
+        expect(() => {calculadora.resultado = 'Text'}).toThrow("O argumento deve ser um numero válido");
 
+    })
+
+    it('Operação soma', () => {
+        const calculadora = new Calculadora();
+        expect(calculadora.soma).toBeDefined();
+        expect(calculadora.soma.length).toBe(1);
     })
 
 });
